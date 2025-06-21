b6dad22143632d834a040382e0aa231d917d43e4
deepak-stackuponline <deepak.stackuponline@gmail.com>
2025-06-21 15:55:14 +0530

    changes

--- ReactJS/project/src/App.js
+++ ReactJS/project/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import AppMenu from './components/header/AppMenu';
 import AppName from './components/header/AppName';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import States from './components/props/States';
 
 
 
@@ -17,6 +18,7 @@ function App() {
 
 <AppMenu/>
 <AppName />
+<States />
 
 </div>
 
