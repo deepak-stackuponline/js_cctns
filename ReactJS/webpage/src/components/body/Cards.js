770d765289301b176e8b3f25de5324221c39e58b
deepak-stackuponline <deepak.stackuponline@gmail.com>
2025-06-25 15:47:56 +0530

    Body

--- ReactJS/webpage/src/components/body/Cards.js
+++ ReactJS/webpage/src/components/body/Cards.js
@@ -1,23 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-function Cards() {
+
+function Cards({book}) {
+  
   return (
-     <div className="card mb-3 mx-3 mt-0" style={{ width: '18rem' }}>
-        <img src="https://placehold.co/600x400" className="card-img-top" alt="Book Cover" />
-        <div className="card-body">
-          <h5 className="card-title">The Great Gatsby</h5>
-          <p className="card-text">
-            by F. Scott Fitzgerald
-          </p>
-         
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">An item</li>
+    <div className="d-flex flex-wrap justify-content-center">
 
-        </ul>
-     
-      </div>
-  )
+        <div className="card m-3 shadow-lg" style={{ width: '18rem' }} key={book.id}>
+          <img
+            src={book.image}
+            className="card-img-top"
+            style={{ height: '180px', objectFit: 'cover' }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{book.title}</h5>
+            <h6 className="text-muted mb-2">by {book.author}</h6>
+
+            <div className="mb-2">
+              {book.genres.map((Hello, i) => (
+                <span key={i} className="badge bg-light text-dark border me-2">{Hello}</span>
+              ))}
+            </div>
+
+            <div className={`mb-3 fw-semibold ${book.available === "Available" ? 'text-success' : 'text-danger'}`}>
+              {book.available}
+            </div>
+            <hr/>
+
+            <div className="d-flex justify-content-between text-muted mb-3">
+              <span>Buy: <strong>${book.buyPrice}</strong></span>
+              <span>Rent: <strong>${book.rentPrice}</strong>/wk</span>
+            </div>
+
+            <button className="btn btn-light w-100 border fw-semibold">View Details</button>
+          </div>
+        </div>
+      
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
--- ReactJS/webpage/src/components/body/FeaturedBooks.js
+++ ReactJS/webpage/src/components/body/FeaturedBooks.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from './Cards';
 
 function FeaturedBooks() {
+  const [books, setBooks] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await fetch("https://mocki.io/v1/eeaf0d91-66bb-4906-ac38-69215b782ca7");
+      const data = await response.json();
+      setBooks(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-        <h4 className='text-center mt-5'>Featured Books</h4>
-        <div className='d-flex justify-content-center m-5'>
+      <h4 className='text-center mt-5'>Featured Books</h4>
 
-
-            <Cards />
-
-            <Cards />
-
-            <Cards />
-        </div>
-
-
-
+      <div className='d-flex flex-wrap justify-content-center container mt-4'>
+        {books.map((data) => (
+          <Cards key={data.id} book={data} />
+        ))}
+      </div>
     </div>
   );
 }
