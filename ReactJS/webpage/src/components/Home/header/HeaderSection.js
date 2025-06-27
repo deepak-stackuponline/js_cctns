7e63552bb2fdd806d0f9efeb399e321997961a97
deepak-stackuponline <deepak.stackuponline@gmail.com>
2025-06-27 16:12:52 +0530

    changes React

--- ReactJS/webpage/src/components/Home/header/HeaderSection.js
+++ ReactJS/webpage/src/components/Home/header/HeaderSection.js
@@ -4,11 +4,14 @@ import AppName from './AppName';
 
 function HeaderSection() {
   return (
+    
     <div className='d-flex justify-content-between align-items-center p-3 headerMenu' >
     <AppName /> 
     <AppMenu />
      
     </div>
+
+
   );
 }
 
